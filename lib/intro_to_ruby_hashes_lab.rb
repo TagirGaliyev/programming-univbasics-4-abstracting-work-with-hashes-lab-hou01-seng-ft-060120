5f90ce0900c39154bf8edb04672f19742b74e9b0
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-14 16:01:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,9 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   if hash[key]
-    puts "Yes"
+    hash[key] += 1
   else
-    puts "No"
+    hash[key] = 1
   end
 end
 h = my_hash_creator(1,2)
